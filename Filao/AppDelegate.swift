84d1813bee68b24129870bbce53992ad2fde6aef
Ben <ben@bens-mbp.home>
2017-06-27 14:00:38 +0000

    Initial Commit

---
+++ Filao/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Filao
+//
+//  Created by Ben on 27/06/2017.
+//  Copyright © 2017 Ben. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
